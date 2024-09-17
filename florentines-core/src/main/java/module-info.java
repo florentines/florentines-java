853948b47f94696198b44e79e1ee1e718ae16baf
Neil Madden <neil.madden@forgerock.com>
2024-09-17 18:51:54 +0100

    Fresh start

--- florentines-core/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module software.pando.florentines.core {
-    requires software.pando.crypto.nacl;
-    requires org.slf4j;
-    requires com.grack.nanojson;
-
-    exports io.florentine;
-    exports io.florentine.caveat;
-}
